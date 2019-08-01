82a6f1673416af55144d482d98fcb5c915be28bc
Kovacs Alex <kovacs.alex26@gmail.com>
2019-08-01 11:19:05 +0300

    Use res.json instead of res.end (login and register endpoints)

--- routes/login.js
+++ routes/login.js
@@ -13,21 +13,21 @@ router.post('/', async (req, res) => {
     const password = req.headers['password'];
 
     // If one of these two is not provided, end the requests
-    if (!user) return res.status(403).end(error(strings.NO_LOGIN));
-    if (!password) return res.status(403).end(error(strings.NO_PASSWORD));
+    if (!user) return res.status(403).json(error(strings.NO_LOGIN));
+    if (!password) return res.status(403).json(error(strings.NO_PASSWORD));
 
     const filter = emailRegex.test(user) ? { email: user } : { username: user };
 
     // Check if the username exists in the database
     const account = await req.db.table('users').filter(filter).run();
-    if (!account[0]) return res.status(403).end(strings.NO_ACCOUNT);
+    if (!account[0]) return res.status(403).json(strings.NO_ACCOUNT);
 
     // Check if the provided password is the correct one
     const pass = await bcrypt.compare(password, account[0].password);
-    if (!pass) return res.status(403).end(error(strings.WRONG_PASSWORD));
+    if (!pass) return res.status(403).json(error(strings.WRONG_PASSWORD));
 
     // Return a token the user will be able to use
-    return res.end(token(jwt.sign({ data: account[0].id, exp: Math.floor(Date.now() / 1000) + (60 * 60) }, jwtSecret)))
+    return res.json(token(jwt.sign({ data: account[0].id, exp: Math.floor(Date.now() / 1000) + (60 * 60) }, jwtSecret)))
 })
 
 const error = (text) => JSON.stringify({ error: text });
