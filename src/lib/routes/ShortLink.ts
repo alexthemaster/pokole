dba77580112de24cec5844d157d03028439ef68a
Kovacs Alex <alex@atm.moe>
2020-07-16 11:44:46 +0300

    chore(ShortLink): add some comments

--- src/lib/routes/ShortLink.ts
+++ src/lib/routes/ShortLink.ts
@@ -6,9 +6,14 @@ const router = Router();
 
 router.get('/:short', async (req, res) => {
     const { short } = req.params;
+
+    // Get the original (unshortened) URL from the database 
     const [data] = await DBQueries.getLink((req as CustomRequest).db, short);
+
+    // Redirect the user to a 404 page if the shortlink doesn't exist
     if (!data) return res.redirect('/404');
 
+    // Redirect the user to the original URL
     else res.redirect(data.original);
 
     // TODO: counters and stuff
