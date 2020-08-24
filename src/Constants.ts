4ea10b72107d18e2ede8f79bb1ad514edc9e39ee
Kovacs Alex <alex@atm.moe>
2020-08-24 22:33:15 +0300

    chore(Constants): remove duplicate character

--- src/Constants.ts
+++ src/Constants.ts
@@ -22,7 +22,7 @@ export const NO_JWT = "Please provide a JWT secret token (used for encrypting us
 export const SERVE_STATIC = (directory: string) => `[Server] Serving static files from directory ${directory}!`;
 
 export const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-export const URLRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+export const URLRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&/=]*)/;
 export const WordRegex = /^[\w\-\_]+$/;
 
 export const REGISTRATION = (enabled: boolean) => { return { enabled } };
